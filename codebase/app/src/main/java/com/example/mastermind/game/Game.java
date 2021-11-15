cdd85e7307b04856de049223b9cc75fc53518aec
unknown <nikhagaming@gmail.com>
2021-11-15 23:13:53 +0100

    NH | added code generation to game

--- codebase/app/src/main/java/com/example/mastermind/game/Game.java
+++ codebase/app/src/main/java/com/example/mastermind/game/Game.java
@@ -3,21 +3,87 @@ package com.example.mastermind.game;
 import android.util.Pair;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
-    private int currentStep;
+    private int currentRound;
     private int codeLength;
+    private int maxRounds;
+    private int variants;
     private int[] code;
+    private boolean doubleValuesAllowed;
+    private boolean gameWon;
+    private boolean gameLost;
 
-    public Game(int rounds, int codeLength){
+    public int getCurrentRound() {
+        return currentRound;
+    }
+
+    public int getCodeLength() {
+        return codeLength;
+    }
+
+    public int getMaxRounds() {
+        return maxRounds;
+    }
+
+    public int getVariants() {
+        return variants;
+    }
+
+    public int[] getCode() {
+        return code;
+    }
+
+    public boolean areDoubleValuesAllowed() {
+        return doubleValuesAllowed;
+    }
+
+    public boolean isGameWon() {
+        return gameWon;
+    }
+
+    public boolean isGameLost() {
+        return gameLost;
+    }
+
+    public Game(int maxRounds, int codeLength, int variants, boolean doubleValuesAllowed) {
         this.codeLength = codeLength;
         this.code = new int[codeLength];
-        currentStep = 0;
+        this.maxRounds = maxRounds;
+        this.variants = variants;
+        currentRound = 0;
+        this.doubleValuesAllowed = doubleValuesAllowed;
+        gameWon = false;
+        gameLost = false;
     }
 
     public void createRandomCode(){
 
+        for (int i = 0; i<codeLength; i++){
+
+            //generate random value
+            int nextValue = ThreadLocalRandom.current().nextInt(variants);
+
+            //if code already contains value, generate more values until value is unique
+            if(!doubleValuesAllowed)
+                while (Arrays.asList(code).contains(nextValue)){
+                    nextValue = ThreadLocalRandom.current().nextInt(variants);
+                }
+
+            code[i] = nextValue;
+        }
+    }
+
+    public void setCustomCode(int[] code) throws Exception {
+        if (code.length == codeLength)
+            this.code = code;
+        else
+            throw new Exception(
+                    "Length of code (" + code.length + ") does not match set code length (" + codeLength + ")"
+            );
     }
 
     /**
@@ -27,15 +93,16 @@ public class Game {
     public Pair<Integer, Integer> validateGuess(int[] guess) throws Exception {
 
         //check if guess is same length as code
-
         if(guess.length != codeLength){
             throw new Exception(
                     "Length of guess (" + guess.length + ") does not match code length (" + codeLength + ")"
             );
         }
 
-        //get number of black and white pins to be returned
+        //set current round
+        currentRound++;
 
+        //get number of black and white pins to be returned
         int black = 0;
         int white = 0;
 
@@ -45,6 +112,12 @@ public class Game {
             else if (Arrays.asList(code).contains(guess[i]))
                 white++;
         }
+
+        //check if game is won or lost
+        if (black == codeLength)
+            gameWon=true;
+        else if (currentRound == maxRounds && gameWon == false)
+            gameLost=true;
 
         return new Pair<>(black, white);
     }
