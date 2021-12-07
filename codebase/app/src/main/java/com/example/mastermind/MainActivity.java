ee9bb1f173a3226695a72a82a2ac7b957d88d9b9
unknown <nikhagaming@gmail.com>
2021-12-07 22:11:37 +0100

    NH | Failed Poc git commit -m NH

--- codebase/app/src/main/java/com/example/mastermind/MainActivity.java
+++ codebase/app/src/main/java/com/example/mastermind/MainActivity.java
@@ -3,12 +3,94 @@ package com.example.mastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TableLayout tableLayout;
+    List<TableRow> rows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instatiate all table rows
+        InstantiateRows(8);
+
+        // Create a ConstraintLayout in which to add the ImageView
+         tableLayout = findViewById(R.id.gameboard);
+
+        // Add the ImageView to the layout and set the layout as the content view.
+        drawPin(1, 1, 1);
+        drawPin(1, 2, 2);
+        drawPin(1, 3, 3);
+        drawPin(1, 4, 4);
+        drawPin(1, 5, 5);
+
+
+        //constraintLayout.addView(gridLayout);
+        //setContentView(constraintLayout);
+
+    }
+
+    public void drawPin(int row, int column, int id){
+        ImageView view = getPinFromID(id);
+        LinearLayout llayout = new LinearLayout(this);
+        llayout.addView(view);
+        TableRow c_row = rows.get(row);
+        tableLayout.addView(llayout);
+    }
+
+    public void InstantiateRows(int count){
+        rows = new ArrayList<TableRow>();
+
+        for (int i = 0; i <count; i++){
+            TableRow row = new TableRow(this);
+            rows.add(row);
+        }
+    }
+
+    public ImageView getPinFromID(int id){
+        ImageView view = new ImageView(this);
+        switch (id){
+            case 1:
+                view.setImageResource(R.drawable.pin_black);
+                break;
+            case 2:
+                view.setImageResource(R.drawable.pin_blue);
+                break;
+            case 3:
+                view.setImageResource(R.drawable.pin_green);
+                break;
+            case 4:
+                view.setImageResource(R.drawable.pin_grey);
+                break;
+            case 5:
+                view.setImageResource(R.drawable.pin_purple);
+                break;
+            case 6:
+                view.setImageResource(R.drawable.pin_red);
+                break;
+            case 7:
+                view.setImageResource(R.drawable.pin_white);
+                break;
+            case 8:
+                view.setImageResource(R.drawable.pin_yellow);
+                break;
+        }
+        view.setAdjustViewBounds(true);
+        view.setX(10);
+        view.setY(10);
+        return view;
     }
 }
